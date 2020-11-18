beabc4e3a3c29f855d8ad5de8f534e86b878e2f8
Bohdan Leshchyshyn <bohdan.leshchyshyn@keenethics.com>
2020-11-18 19:24:12 +0200

    Deploy to google cloud

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t bohdanleshchyshyn/multi-client:latest -t bohdanleshchyshyn/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t bohdanleshchyshyn/multi-server:latest -t bohdanleshchyshyn/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t bohdanleshchyshyn/multi-worker:latest -t bohdanleshchyshyn/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push bohdanleshchyshyn/multi-client:latest
+docker push bohdanleshchyshyn/multi-server:latest
+docker push bohdanleshchyshyn/multi-worker:latest
+
+docker push bohdanleshchyshyn/multi-client:$SHA
+docker push bohdanleshchyshyn/multi-server:$SHA
+docker push bohdanleshchyshyn/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=bohdanleshchyshyn/multi-client:SHA
+kubectl set image deployments/server-deployment server=bohdanleshchyshyn/multi-server:SHA
+kubectl set image deployments/worker-deployment worker=bohdanleshchyshyn/multi-worker:SHA
