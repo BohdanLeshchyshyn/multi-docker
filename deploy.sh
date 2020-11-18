e635d401a30d26e5e51ee24e0d566a7ce9b1efdd
Bohdan Leshchyshyn <bohdan.leshchyshyn@keenethics.com>
2020-11-18 20:39:13 +0200

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ docker push bohdanleshchyshyn/multi-server:$SHA
 docker push bohdanleshchyshyn/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=bohdanleshchyshyn/multi-client:SHA
-kubectl set image deployments/server-deployment server=bohdanleshchyshyn/multi-server:SHA
-kubectl set image deployments/worker-deployment worker=bohdanleshchyshyn/multi-worker:SHA
+kubectl set image deployments/client-deployment client=bohdanleshchyshyn/multi-client:$SHA
+kubectl set image deployments/server-deployment server=bohdanleshchyshyn/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=bohdanleshchyshyn/multi-worker:$SHA
